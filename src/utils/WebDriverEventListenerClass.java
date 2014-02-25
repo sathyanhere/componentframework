d19978e6315e3b12cced6f0adc2202a14da58b6d
sundaram.pradeep <sundaram.pradeep@gmail.com>
2014-02-25 13:26:59 +0000

    changed the comment for error page

--- src/utils/WebDriverEventListenerClass.java
+++ src/utils/WebDriverEventListenerClass.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 
-import pages.WebPage;
 import reports.Report;
 
 public class WebDriverEventListenerClass extends AbstractWebDriverEventListener{
@@ -21,10 +20,10 @@ public class WebDriverEventListenerClass extends AbstractWebDriverEventListener{
 			File directory = new File(".");
 			String path = directory.getCanonicalPath()+ "\\test-output\\screenshot";
 			File f=new File(path + "\\failure.png");
-        	/*File scrFile = ((TakesScreenshot)WebPage.driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile, f);*/
+        	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, f);
 			String hyperLink=ScreenShotInitial+"failure"+ScreenShotEnd;
-			Report.log("Error in this page "+hyperLink+"<BR>");
+			Report.log("\""+throwable.getClass().toString()+"\" error in this page "+hyperLink+"<BR>");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
