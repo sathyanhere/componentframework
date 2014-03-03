a9be13be7a9b04fc37297d128dde0296f7c19db1
sundaram.pradeep <sundaram.pradeep@gmail.com>
2014-03-03 12:38:15 +0000

    Changes made to handle chrome, chrome driver and ie drivers are parameterized

--- src/browser/Browser.java
+++ src/browser/Browser.java
@@ -1,5 +1,6 @@
 package browser;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -9,7 +10,8 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
 import utils.WebDriverEventListenerClass;
 
 public class Browser {
-
+	public static String IEDriverPath=null;
+	public static String ChromeDriverPath=null;
 	public static EventFiringWebDriver getInstance(String browerType) {
 		WebDriverEventListenerClass eventListener = new WebDriverEventListenerClass();
 		
@@ -22,9 +24,16 @@ public class Browser {
 			return driver;
 		} 
 		else if (browerType.equals("ie")) {
-			System.setProperty("webdriver.ie.driver","D:\\selenium drivers\\IEDriverServer.exe");
+			System.out.println("driver path is "+IEDriverPath);
+			System.setProperty("webdriver.ie.driver",IEDriverPath);
 			DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
 			EventFiringWebDriver driver = new EventFiringWebDriver(new InternetExplorerDriver(ieCapabilities));
+			driver.register(eventListener);
+			return driver;
+		}
+		else if (browerType.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",ChromeDriverPath);
+			EventFiringWebDriver driver = new EventFiringWebDriver(new ChromeDriver());
 			driver.register(eventListener);
 			return driver;
 		}
